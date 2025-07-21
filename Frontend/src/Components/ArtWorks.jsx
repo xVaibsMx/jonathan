0dff855f8dbdaac812b4949aa6cabc3aedffff99
MxVaibs <mvaibhav.main@gmail.com>
2025-07-21 21:46:22 +0530

    Changes committed.

--- Frontend/src/Components/ArtWorks.jsx
+++ Frontend/src/Components/ArtWorks.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const ArtWorks = () => {
   return (
     <section className=" mt-40 max-w-7xl mx-auto">
-      <h1 className=" text-3xl ">ArtWorks</h1>
-      <h2>Some of my works.</h2>
+      <h1 className=" text-5xl font-bold ">ArtWorks</h1>
+      <h2 className=" text-xl font-light mt-3">Some of my works.</h2>
+      
     </section>
   )
 }
---
+++ Frontend/src/Data/artworks.js
@@ -0,0 +1,21 @@
+import Art1 from '../Assets/Drawing1.jpeg'
+import Art2 from '../Assets/Drawing2.jpeg'
+import Art3 from '../Assets/Drawing3.jpeg'
+import Art4 from '../Assets/Drawing4.jpeg'
+import Art5 from '../Assets/Drawing5.jpeg'
+import Art6 from '../Assets/Drawing6.jpeg'
+import Art7 from '../Assets/Drawing7.jpeg'
+import Art8 from '../Assets/Drawing8.jpeg'
+
+const artWorks = [
+  { Name: 'Art 1', img: Art1 },
+  { Name: 'Art 2', img: Art2 },
+  { Name: 'Art 3', img: Art3 },
+  { Name: 'Art 4', img: Art4 },
+  { Name: 'Art 5', img: Art5 },
+  { Name: 'Art 6', img: Art6 },
+  { Name: 'Art 7', img: Art7 },
+  { Name: 'Art 8', img: Art8 },
+]
+
+export default artWorks
